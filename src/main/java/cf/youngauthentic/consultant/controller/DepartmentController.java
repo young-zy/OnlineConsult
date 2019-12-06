e8fa19e73f64dae0ac38911edfa0cb4b10050067
young-zy <youngzhaozhou@gmail.com>
2019-12-06 09:06:23 +0800

    update department controller

--- src/main/java/cf/youngauthentic/consultant/controller/DepartmentController.java
+++ src/main/java/cf/youngauthentic/consultant/controller/DepartmentController.java
@@ -1,12 +1,14 @@
 package cf.youngauthentic.consultant.controller;
 
-import cf.youngauthentic.consultant.model.DepartmentEntity;
+import cf.youngauthentic.consultant.model.ResponseModel;
 import cf.youngauthentic.consultant.service.DepartmentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 public class DepartmentController {
@@ -15,8 +17,21 @@ public class DepartmentController {
     private DepartmentService departmentService;
 
     @GetMapping(path = "/department")
-    public List<DepartmentEntity> getDepartments() {
-        return departmentService.getDepartments();
+    public ResponseEntity<Object> getDepartments(@RequestHeader(defaultValue = "") String token) {
+        try {
+            return new ResponseEntity<>(departmentService.getDepartments(token), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(new ResponseModel(false, e.getMessage()), HttpStatus.FORBIDDEN);
+        }
+    }
+
+    @GetMapping(path = "/department/{id}")
+    public ResponseEntity<Object> getDepartment(@PathVariable int id, @RequestHeader(defaultValue = "") String token) {
+        try {
+            return new ResponseEntity<>(departmentService.getDepartment(id), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(new ResponseModel(false, e.getMessage()), HttpStatus.FORBIDDEN);
+        }
     }
 
 }
