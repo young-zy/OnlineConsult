9326ab5fed376f485265c25d932b8589817f0bbf
young-zy <youngzhaozhou@gmail.com>
2019-12-11 13:34:46 +0800

    Revert override

--- src/main/java/cf/youngauthentic/consultant/repo/DepartmentRepo.java
+++ src/main/java/cf/youngauthentic/consultant/repo/DepartmentRepo.java
@@ -4,6 +4,5 @@ import cf.youngauthentic.consultant.model.DepartmentEntity;
 import org.springframework.data.repository.CrudRepository;
 
 public interface DepartmentRepo extends CrudRepository<DepartmentEntity, Integer> {
-    @Override
-    Iterable<DepartmentEntity> findAll();
+
 }
